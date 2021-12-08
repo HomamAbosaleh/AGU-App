917ee04f19cbfba3a0d983bd07789bf846a65ac2
KugelblitZ-121 <82136584+KugelblitZ-121@users.noreply.github.com>
2021-12-08 21:58:38 +0300

    Update payment.dart

--- final_project/lib/UI/canteen/payment.dart
+++ final_project/lib/UI/canteen/payment.dart
@@ -1,95 +1,105 @@
+import 'package:final_project/services/firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-class payments extends StatefulWidget {
-  int? money;
-  payments({this.money});
+class Payments extends StatefulWidget {
+  const Payments({Key? key}) : super(key: key);
+  @override
+  _PaymentsState createState() => _PaymentsState();
+}
+
+class _PaymentsState extends State<Payments> {
+  final balanceController = TextEditingController();
+  double balance = 0;
+  void addMoney(double newBalance) {
+    balance += newBalance;
+  }
+
+  Stream? student;
+
+  getStudent() async {
+    FireStore().getStudentStream().then((value) {
+      setState(() {
+        student = value;
+      });
+    });
+  }
 
   @override
-  _paymentsState createState() => _paymentsState();
-}
-
-double balance = 0;
-void addMoney(double newBalance) {
-  balance += newBalance;
-}
-
-final balanceController = TextEditingController();
-
-class _paymentsState extends State<payments> {
-  // Widget buildSheet() {
-  //   return Container(
-  //     height: 200,
-  //     child: Column(
-  //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  //       children: [
-  //         Container(
-  //           padding: EdgeInsets.all(10),
-  //           child: TextField(
-  //             controller: balanceController,
-  //             decoration: InputDecoration(label: Text('balance')),
-  //           ),
-  //         ),
-  //         TextButton(
-  //             onPressed: () {
-  //               balance++;
-  //               Navigator.of(context).pop();
-  //             },
-  //             child: Text('Add Money'))
-  //       ],
-  //     ),
-  //   );
-  // }
+  void initState() {
+    getStudent();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Amount of Money in Card: '),
-          SizedBox(height: 5),
-          Text('₺ $balance', style: TextStyle(fontSize: 30)),
-          SizedBox(height: 30),
-          ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor:
-                  MaterialStateColor.resolveWith((states) => Colors.white),
+    return StreamBuilder(
+      stream: student,
+      builder: (context, AsyncSnapshot snapShot) {
+        if (snapShot.data != null) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Amount of Money in Card: '),
+                const SizedBox(height: 5),
+                Text('${snapShot.data['wallet']}',
+                    style: const TextStyle(fontSize: 30)),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateColor.resolveWith(
+                        (states) => Colors.white),
+                  ),
+                  onPressed: () {
+                    showBottomSheet(
+                        context: context,
+                        builder: (context) => buildSheet(snapShot));
+                  },
+                  child: const Icon(Icons.account_balance_wallet,
+                      color: Colors.black),
+                ),
+              ],
             ),
-            onPressed: () {
-              showBottomSheet(
-                  context: context, builder: (context) => buildSheet());
-            },
-            child:
-                const Icon(Icons.account_balance_wallet, color: Colors.black),
-          ),
-        ],
-      ),
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 
-  Widget buildSheet() {
-    return Container(
+  Widget buildSheet(AsyncSnapshot snapShot) {
+    return SizedBox(
       height: 200,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Container(
-            padding: EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
             child: TextField(
               controller: balanceController,
-              decoration: InputDecoration(label: Text('balance')),
+              decoration: const InputDecoration(label: Text('balance')),
             ),
           ),
           TextButton(
-              onPressed: () {
-                addMoney(double.parse(balanceController.text));
-                Navigator.of(context).pop();
-              },
-              child: const Text(
-                'Add Money',
-                style: TextStyle(fontSize: 20),
-              ))
+            onPressed: () {
+              setState(
+                () {
+                  double newBalance = snapShot.data['wallet'] +
+                      double.parse(balanceController.text);
+                  FireStore().addMoney(newBalance);
+                },
+              );
+              Navigator.of(context).pop();
+            },
+            child: const Text(
+              'Add Money',
+              style: TextStyle(fontSize: 20),
+            ),
+          )
         ],
       ),
     );
