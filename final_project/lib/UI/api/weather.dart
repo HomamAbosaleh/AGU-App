54f23d1f72f7f5c9a0d2ff03d338ca39b085fa5d
HAMMAMABS <hammam.abosaleh@outlook.com>
2021-12-20 19:40:19 +0300

    APIs

--- final_project/lib/UI/api/weather.dart
+++ final_project/lib/UI/api/weather.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 
+import '../../services/http.dart';
+
 class Weather extends StatelessWidget {
   const Weather({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(child: Text('Weather')),
+    return FutureBuilder(
+      future: Http().getWeatherByLocation(),
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          return Center(
+            child: Card(
+              child: Column(
+                children: [
+                  Text(snapshot.data),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
