2d32182a4153e6a08c976873e14dba7dd07d8975
foo-ab <92823560+foo-ab@users.noreply.github.com>
2021-12-28 18:27:45 +0300

    Update currency.dart

--- final_project/lib/UI/api/currency.dart
+++ final_project/lib/UI/api/currency.dart
@@ -1,112 +1,40 @@
-import 'package:final_project/theme/theme.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../services/http.dart';
 
-class Weather extends StatelessWidget {
-  Weather({Key? key}) : super(key: key);
-
-  var weatherinfo = Http().getWeatherByLocation();
+class Currency extends StatefulWidget {
+  const Currency({Key? key}) : super(key: key);
 
   @override
+  State<Currency> createState() => _CurrencyState();
+}
+
+class _CurrencyState extends State<Currency> {
+  dynamic currency;
+  @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
+    Http().getCurrency();
     return FutureBuilder(
-      future: weatherinfo,
+      future: Http().getCurrencies(),
       builder: (context, AsyncSnapshot snapshot) {
         if (snapshot.hasData) {
-          print(snapshot.data);
-          return SizedBox(
-            height: height,
-            width: width,
-            child: Center(
-            // child: Text(snapshot.data.toString()),
-             child: Card(
-               color: Theme.of(context).cardTheme.color,
-               child: Column(
-                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                 children: [
-                   Expanded(
-                   child: Column(
-                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                     children: [
-                       Text(snapshot.data.locationName, style: Theme.of(context).textTheme.headline3),
-                       Icon(Http().getWeatherIcon(snapshot.data.weatherID),size: 50,color: Theme.of(context).appBarTheme.iconTheme!.color),
-                       SizedBox(height: 5),
-                       Text(snapshot.data.temperature.toString()+"°",style: Theme.of(context).textTheme.headline3),
-                       SizedBox(height: 5),
-                       Text("Feels Like: "+snapshot.data.feelsLike.toString()+"°",style: Theme.of(context).textTheme.headline1),
-                       SizedBox(height: 5),
-                     ],
-                   ),
-                 ),
-                   Expanded(
-                     child: Row(
-                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                       children: [
-                         Column(
-                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                           children: [
-                             Row(
-                                 children: [
-                                   Text("Min ", style: Theme.of(context).textTheme.headline2),
-                                   Icon(FontAwesomeIcons.temperatureLow,color: Colors.blue),
-                                 ]
-                             ),
-                             Text(snapshot.data.lowTemp.toString()+"°", style: Theme.of(context).textTheme.headline1),
-                             Row(
-                                 children:[
-                                   Text("Wind Speed ",style: Theme.of(context).textTheme.headline1),
-                                   Icon(FontAwesomeIcons.wind,color: Theme.of(context).appBarTheme.iconTheme!.color)
-                                 ]
-                             ),
-                             Text(snapshot.data.windSpeed.toString(), style: Theme.of(context).textTheme.headline2),
-                             Row(
-                               children: [
-                                 Text("Visibility ",style: Theme.of(context).textTheme.headline1),
-                                 Icon(FontAwesomeIcons.eye,color: Theme.of(context).appBarTheme.iconTheme!.color),
-                               ],
-                             ),
-                             Text((snapshot.data.visibility/1000).toStringAsFixed(2)+"km",style: Theme.of(context).textTheme.headline1),
-                           ]
-                         ),
-                         Column(
-                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                           crossAxisAlignment: CrossAxisAlignment.center,
-                           children: [
-                             Row(
-                                 children: [
-                                   Text("Max ", style: Theme.of(context).textTheme.headline2),
-                                   Icon(FontAwesomeIcons.temperatureHigh,color: rPrimaryRedColor)
-                                 ]
-                             ),
-                             Text(snapshot.data.highTemp.toString()+"°", style: Theme.of(context).textTheme.headline1),
-                             Row(
-                                 children: [
-                                   Text("Humidity ",style: Theme.of(context).textTheme.headline1),
-                                   Icon(FontAwesomeIcons.tint,color: Colors.blue)
-                                 ]
-                             ),
-                             Text(snapshot.data.humidity.toString()+"%", style: Theme.of(context).textTheme.headline2),
-                             Row(
-                               children: [
-                                 Text("Pressure ",style: Theme.of(context).textTheme.headline1),
-                                 Icon(FontAwesomeIcons.compressArrowsAlt,color: Theme.of(context).appBarTheme.iconTheme!.color)
-                               ],
-                             ),
-                             Text(snapshot.data.pressure.toString(),style:Theme.of(context).textTheme.headline1),
-                           ],
-                         ),
-                       ],
-                     ),
-                   ),
-                   SizedBox(height: 15)
-                 ],
-               ),
-             ),
+          return Center(
+            child: Card(
+              child: Column(
+                children: [
+                  DropdownButtonFormField(
+                    value: currency,
+                    items: snapshot.data.keys
+                        .map<DropdownMenuItem<Object>>(dropDownBuilder)
+                        .toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        currency = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
             ),
           );
         } else {
@@ -119,3 +47,6 @@ class Weather extends StatelessWidget {
   }
 }
 
+DropdownMenuItem<Object> dropDownBuilder(item) {
+  return DropdownMenuItem(value: item, child: Text(item));
+}
